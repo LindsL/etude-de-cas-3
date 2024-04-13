2f4037b30017831e3277f16aa8bf950d4138c83a
Lindsay <picture-w2c@MacBook-Pro-15-PB.local>
2024-04-13 14:09:01 +0200

    echec fix - tests + config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,22 @@
 module.exports = {
   apps: [
     {
-      name: "app",
-      script: "./www/app.js",
-      env_production: {
-        NODE_ENV: "production",
+      name: 'app',
+      script: 'npm',
+      args: 'start',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '200M',
+      error_file: './logs/err.log',
+      out_file: './logs/out.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
+      env: {
+        NODE_ENV: 'development'
       },
-    },
-  ],
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    }
+  ]
 };
